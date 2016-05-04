1ec58b12047937ec99ab10fb99d09b91523ef1ba
klarkc <walker@praiseweb.com.br>
2016-05-04 11:30:28 -0300

    Adiciona classe HtmlBuilder

---
+++ WEB-INF/classes/livraria/HtmlBuilder.java
@@ -0,0 +1,19 @@
+package livraria;
+
+
+
+public class HtmlBuilder {
+    StringBuilder html = new StringBuilder();
+
+    public void append(Object str) {
+        html.append(str);
+    }
+    public void appendLine(Object str) {
+        html.append("\n");
+        html.append(str);
+    }
+
+    public String toString() {
+        return html.toString();
+    }
+}
