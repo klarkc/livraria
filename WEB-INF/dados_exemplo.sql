2e13645b310e9464ab72dae744e0eda336f481db
KlarkC <walker@praiseweb.com.br>
2016-05-09 12:06:28 -0300

    adiciona arquivo README e arquivos de sql

---
+++ WEB-INF/dados_exemplo.sql
@@ -0,0 +1,136 @@
+CREATE DATABASE  IF NOT EXISTS `claretiano` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `claretiano`;
+-- MySQL dump 10.13  Distrib 5.5.49, for debian-linux-gnu (x86_64)
+--
+-- Host: praiseweb.com.br    Database: claretiano
+-- ------------------------------------------------------
+-- Server version	5.5.49-0+deb7u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `editora`
+--
+
+DROP TABLE IF EXISTS `editora`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `editora` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(60) DEFAULT NULL,
+  `cidade` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `editora`
+--
+
+LOCK TABLES `editora` WRITE;
+/*!40000 ALTER TABLE `editora` DISABLE KEYS */;
+INSERT INTO `editora` VALUES (1,'Abril','São Paulo'),(2,'Globo','Rio de Janeiro'),(3,'Marvel','San Diego'),(4,'DC Comics','California'),(5,'CNN','Washington');
+/*!40000 ALTER TABLE `editora` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `livro`
+--
+
+DROP TABLE IF EXISTS `livro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `livro` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(120) NOT NULL,
+  `autor` varchar(60) DEFAULT NULL,
+  `ano` int(11) DEFAULT NULL,
+  `preco` double DEFAULT NULL,
+  `foto` varchar(45) DEFAULT NULL,
+  `idEditora` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_livro_editora_idx` (`idEditora`),
+  CONSTRAINT `fk_livro_editora` FOREIGN KEY (`idEditora`) REFERENCES `editora` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `livro`
+--
+
+LOCK TABLES `livro` WRITE;
+/*!40000 ALTER TABLE `livro` DISABLE KEYS */;
+INSERT INTO `livro` VALUES (1,'Bela Cozinha','Vela Gil',2016,25.6,'img/livro1.webp',2),(2,'Mais Rápido e Melhor','Charles Duhigg',2016,29.9,'img/livro2.webp',1),(3,'Deadpool','Marvel',2016,15.49,'img/livro3.webp',3),(4,'Manual de Direito Civil','Flávio Tartuce',2015,120,'img/livro4.webp',2),(5,'Before','Anna Todd',2016,39.9,'img/livro5.webp',2),(6,'Mulher Maravilha','DC Comics',2015,14.9,'img/livro6.webp',4);
+/*!40000 ALTER TABLE `livro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quiz_usuario`
+--
+
+DROP TABLE IF EXISTS `quiz_usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quiz_usuario` (
+  `nome_usuario` varchar(10) NOT NULL,
+  `senha` varchar(8) NOT NULL,
+  `primeiro_nome` varchar(15) NOT NULL,
+  `sobrenome` varchar(15) NOT NULL,
+  `sexo` char(1) NOT NULL,
+  PRIMARY KEY (`nome_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quiz_usuario`
+--
+
+LOCK TABLES `quiz_usuario` WRITE;
+/*!40000 ALTER TABLE `quiz_usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `quiz_usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) DEFAULT NULL,
+  `senha` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (0,'klarkc','1234'),(1,'admin','1234');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-05-09 11:55:14
